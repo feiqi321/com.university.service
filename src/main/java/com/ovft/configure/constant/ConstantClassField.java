397703a9136dff440df07a0103b6df9fd44e067b
lidong <502056142@qq.com>
2019-04-13 16:41:31 +0800

    redis缓存失效时间

--- src/main/java/com/ovft/configure/constant/ConstantClassField.java
+++ src/main/java/com/ovft/configure/constant/ConstantClassField.java
@@ -5,7 +5,7 @@ public class ConstantClassField {
     /**
      * redis  pc端token缓存过期时间   30分钟
      */
-    public static final long PC_CACHE_EXPIRATION_TIME = 30*60*1000;
+    public static final long PC_CACHE_EXPIRATION_TIME = 30*60;
 
 
 }
