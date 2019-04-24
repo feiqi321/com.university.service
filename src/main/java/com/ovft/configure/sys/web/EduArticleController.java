a5791beced37469572ca8ff158cbed8fc6f157b4
xia <1657728393@qq.com>
2019-04-24 09:54:28 +0800

    EduArticle

--- src/main/java/com/ovft/configure/sys/web/EduArticleController.java
+++ src/main/java/com/ovft/configure/sys/web/EduArticleController.java
@@ -4,6 +4,7 @@ import com.ovft.configure.http.result.StatusCode;
 import com.ovft.configure.http.result.WebResult;
 import com.ovft.configure.sys.bean.EduArticle;
 import com.ovft.configure.sys.service.EduArticleService;
+import com.ovft.configure.sys.vo.PageVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,5 +33,59 @@ public class EduArticleController {
         return new WebResult(StatusCode.OK, "查询成功", eduArticles);
     }
 
+    /**
+     * 后台添加文章
+     *
+     * @param eduArticle
+     * @return
+     */
+    @PostMapping(value = "server/adminAddNotice")
+    public WebResult adminAddNotice(@RequestBody EduArticle eduArticle) {
+        return eduArticleService.adminAddNotice(eduArticle);
+    }
+
+    /**
+     * 后台查询文章（分页）
+     *
+     * @param type
+     * @return TODO
+     */
+    @PostMapping(value = "server/findNoticeAll")
+    public WebResult findNoticeAll(@RequestParam(value = "type") String type, @RequestBody PageVo pageVo) {
+        return eduArticleService.findNoticeAll(type,pageVo);
+    }
+
+    /**
+     * 后台删除文章
+     *
+     * @param id
+     * @return TODO
+     */
+    @PostMapping(value = "server/deleteNotice")
+    public WebResult deleteNotice(@RequestParam(value = "id") Integer id) {
+        return eduArticleService.deleteNotice(id);
+    }
+
+    /**
+     * 后台批量删除文章
+     *
+     * @param ids
+     * @return TODO
+     */
+    @PostMapping(value = "server/bigDeleteNotice")
+    public WebResult bigDeleteNotice(@RequestParam Integer [] ids) {
+        return eduArticleService.bigDeleteNotice(ids);
+    }
+
+    /**
+     * 后台修改文章
+     *
+     * @param eduArticle
+     * @return
+     */
+    @PostMapping(value = "server/updateNotice")
+    public WebResult updateNotice(@RequestBody EduArticle eduArticle) {
+        return eduArticleService.updateNotice(eduArticle);
+    }
 
 }
