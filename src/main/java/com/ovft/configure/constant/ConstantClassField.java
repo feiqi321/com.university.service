de647cec478682117379689654a342027a2f48a1
lidong <502056142@qq.com>
2019-05-07 09:37:32 +0800

    后台token缓存时间修改

--- src/main/java/com/ovft/configure/constant/ConstantClassField.java
+++ src/main/java/com/ovft/configure/constant/ConstantClassField.java
@@ -3,9 +3,9 @@ package com.ovft.configure.constant;
 public class ConstantClassField {
 
     /**
-     * redis  pc端token缓存过期时间   30分钟
+     * redis  pc端token缓存过期时间   //一天
      */
-    public static final long PC_CACHE_EXPIRATION_TIME = 30*60;
+    public static final long PC_CACHE_EXPIRATION_TIME = 24*60*60;
 
     /**
      * redis  单点登录功能key   single sign on   SSO
