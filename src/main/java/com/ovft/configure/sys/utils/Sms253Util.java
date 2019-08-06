11154a209f0e948e4c267f4e0f8ee7bbcef882a0
123450zy <1657728393@qq.com>
2019-08-06 16:44:42 +0800

    "注释密码校验"

--- src/main/java/com/ovft/configure/sys/utils/Sms253Util.java
+++ src/main/java/com/ovft/configure/sys/utils/Sms253Util.java
@@ -26,9 +26,9 @@ public class Sms253Util {
 
     private static final String charset = "utf-8";
     // 用户平台API账号(非登录账号,示例:N1234567)
-    private static final String account = "";
+    private static final String account = "N4066451";
     // 用户平台API密码(非登录密码)
-    private static final String password = "";
+    private static final String password = "goXkQq2taY4c63";
     //请求地址请登录253云通讯自助通平台查看或者询问您的商务负责人获取
     private static final String smsSingleRequestServerUrl = "http://smssh1.253.com/msg/send/json";
 
